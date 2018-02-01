c084fc6b0f75aa3b80becaf34af61331f0e0c3d5
Johan Anderstedt <jumpi_inn@hotmail.com>
2018-02-01 15:01:07 +0100

    Trafic Light 0.1

---
+++ Grupp2PuzzleRunner/Assets/Scripts/TraficLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TraficLight : MonoBehaviour {
+	int curScore = 0;
+	int maxScore = 100;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown ("Space") && curScore < maxScore)
+		{
+			curScore = curScore + 5;
+			Debug.Log (curScore);
+			if (curScore > 0 && curScore != maxScore) 
+			{
+				Debug.Log ("H");
+			}
+		}
+	}
+
+}
